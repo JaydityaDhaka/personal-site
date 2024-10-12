04eda7858c61f3e249ea691ee066da7df928dc68
JaydityaDhaka <dhakajayditya@gmail.com>
2024-10-12 06:59:36 +0530

    S1.2 made changes across all the pages

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,47 +1,55 @@
 // TODO Add a couple lines about each project
 const data = [
   {
-    title: 'Nearest Dollar',
-    subtitle: '2015 BVP Hackathon',
-    image: '/images/projects/nearestdollar.jpg',
-    date: '2015-11-20',
+    title: 'Terminal-based RPG',
+    subtitle: 'Personal Project',
+    image: '/images/projects/DragonQuest.jpg',
+    date: '2024-10-09',
     desc:
-      'Built for a social impact hackathon. '
-      + 'NearestDollar connected to your bank accounts, credit cards, '
-      + 'or debit cards and rounded up your purchases to donate the balance to '
-      + 'the charity of your choice.',
+      'Built as a personal side project. '
+      + 'Choose a role which suits you, '
+      + 'Kill mobs, explore the in game world and upgrade your stats  '
+      + 'Kill the dragon to win the game, but beware you only have 1 life',
   },
   {
-    title: 'Harvest',
-    subtitle: 'Won 3rd. place in 2015 Techcrunch Disrupt SF Hackathon',
-    link: 'https://devpost.com/software/harvest',
-    image: '/images/projects/harvest.jpg',
-    date: '2015-09-20',
-    desc:
-      'Won ~ $7000 in prizes for an advanced, low cost monitoring solution '
-      + 'for crops. Harvest was designed to catch irrigation leaks, overwatering, '
-      + 'and nutrient deficiencies at an affordable price for the developing world.',
-  },
-  {
-    title: 'Space Potato',
-    subtitle: 'A kickstarter funded potato powered weather balloon.',
+    title: 'Dynamic Ambulance Placement in Bengaluru City ',
+    subtitle: '7th semester Project.',
     link: 'http://www.spacepotato.org',
-    image: '/images/projects/spacepotato.jpg',
-    date: '2015-06-28',
+    image: '/images/projects/ambulance.jpg',
+    date: '2024-07-20',
     desc:
-      'Launched a potato battery powered weather balloon with two cameras '
-      + 'and gps transponder. Resulting photos were published in a coffee table book. '
-      + 'You can email me for a copy.',
+      'Placing ambulance dynamically in the city based on certain parameters'
+      + 'Helps by reducing the response time and increasing ambulance availability ',
   },
   {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
+    title: 'Ciphers',
+    subtitle: 'Personal Project ',
+    link: 'https://devpost.com/software/harvest',
+    image: '/images/projects/ciphers.jpg',
+    date: '2024-06-20',
     desc:
-      'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-      + 'Over 60,000 cats were classified before server bills made the project too expensive '
-      + 'to continue hosting.',
+      'Next JS based Encryption and Decryption Application, '
+      + 'Currently Supports Caesar cipher and Playfair cipher  '
+      + 'Future scope is to add complex Encryption Algos.',
+  },
+  {
+    title: 'On-line: A video streaming platform',
+    subtitle: 'a fullstack NEXTJS project.',
+    link: 'http://www.spacepotato.org',
+    image: '/images/projects/OnLine.jpg',
+    date: '2024-07-20',
+    desc:
+      'Created a Real time streaming platform using React, MongoDB, and Tailwind CSS,'
+      + 'featuring user authentication, real-time database integration, '
+      + ' and a responsive UI for browsing and streaming movies and TV shows.',
+  },
+  {
+    title: 'HangMan',
+    subtitle: 'CLI based game',
+    image: '/images/projects/Hangman.jpg',
+    date: '2024-09-15',
+    desc:
+      'Fine side project to keep myself occupied',
   },
 ];
 
