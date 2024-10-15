a1ef4ec239fcddd6ad023606c363f24a1dc0c6d3
JaydityaDhaka <dhakajayditya@gmail.com>
2024-10-16 02:23:43 +0530

    V1 - Correctd all profile Info, launch

--- src/data/resume/work.js
+++ src/data/resume/work.js
@@ -20,9 +20,9 @@ const work = [
     endDate: '2023-11-10',
     summary: `Part of team Ingestwise which consits of roughly 20 highly skilled members, the team focuses primarily on connected vehicle platform. I got to work on the project by utilising various cloud Security concepts like AWS IAM and Amazon Cognito, also created and managed an API Gateway for smooth and secure flow of the numerous APIs being used in the project using Amazon API Gateway. 
     Additionally researched and worked on Eclipse foundation projects linked to the automotive industry particularly Eclipse kuksa which is an open source software still in development with major contributions from tech giants like BMW, Bosch GmbH, etc.`,
-    highlights: [
-      '', // need to add highlights
-    ],
+    // highlights: [
+    //   '', // need to add highlights
+    // ],
   },
   {
     name: 'Tata Elxsi',
@@ -32,9 +32,9 @@ const work = [
     endDate: '2024-07-15',
     summary: `Established a networking layer for a client-server architecture made using Java and utilising HTTPs methods, also created a project specific packet structure for sending data to a MySQL database while working on persistence.
     I was also introduced to the concepts of Connected cars, Software defined vehicles and the importance of IOT & Cloud technologies in these conecpts.`,
-    highlights: [
-      '', // need to add highlights
-    ],
+    // highlights: [
+    //   '', // need to add highlights
+    // ],
   },
 ];
 
